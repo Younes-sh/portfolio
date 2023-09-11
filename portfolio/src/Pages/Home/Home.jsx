4416252e763e05237f14d2f4d9088e6bb56da113
Younes-sh <sheikhlaryounes@gmail.com>
2023-09-11 14:15:25 +0200

    Update Home

--- portfolio/src/Pages/Home/Home.jsx
+++ portfolio/src/Pages/Home/Home.jsx
@@ -18,21 +18,24 @@ const ContentBack = styled.div`
   background-image: url('https://i.pinimg.com/originals/af/a9/24/afa92424317d162894bd1379823b7fdc.gif');
   background-position: center;
   background-size: cover;
+  position: fixed;
 `
 const ContentFront = styled.div`
   width: 100%;
   height: 100vh;
-  background-color: #00ffff8d;
+  background-color: #0816168d;
   border: solid .1rem;
   position: absolute;
   z-index: 2000;
+  backdrop-filter: blur( 4px );
+  -webkit-backdrop-filter: blur( 4px );
 `
 
 const Home = () => {
   return (
     <Container>
       <ContentBack>
-        back
+        <img src="" alt="" />
       </ContentBack>
 
       <ContentFront>
