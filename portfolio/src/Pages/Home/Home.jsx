80a8a2742f4f0dd5d6ef4b26cc5b51f81e886d6f
Younes-sh <sheikhlaryounes@gmail.com>
2023-09-11 11:36:57 +0200

    install style-components

--- portfolio/src/Pages/Home/Home.jsx
+++ portfolio/src/Pages/Home/Home.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: #BF4F74;
+`;
 
 const Home = () => {
+
+
+
   return (
     <div>
-        <h1>Home</h1>
+        <Title>Home</Title>
     </div>
   )
 }
