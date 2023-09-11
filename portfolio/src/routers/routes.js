abb71f4841ca8e86b26d278080ed724196f185b0
Younes-sh <sheikhlaryounes@gmail.com>
2023-09-11 11:24:05 +0200

    Create routes

---
+++ portfolio/src/routers/routes.js
@@ -0,0 +1,11 @@
+import Home from "../Pages/Home/Home";
+import About from "../Pages/About/About";
+import Contact from "../Pages/Contact/Contact";
+
+const routes  = [
+    { path:'/' , element:<Home />},
+    { path:'/about' , element:<About />},
+    { path:'/contact' , element:<Contact />},
+]
+
+export default routes
