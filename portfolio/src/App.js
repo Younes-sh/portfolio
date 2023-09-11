02e6219966e5ca169adecebf24ab6341c30ea15e
Younes-sh <sheikhlaryounes@gmail.com>
2023-09-11 14:08:53 +0200

    Modified App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Routing from './routers/Routing';
+import Navbar from './Components/Navbar/Navbar';
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+          <Navbar />
         <Routing />
       </BrowserRouter>
     </div>
