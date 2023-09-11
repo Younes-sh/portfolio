ab7822712ff0ebea666106ed2bf939e41023e663
Younes-sh <sheikhlaryounes@gmail.com>
2023-09-11 14:09:22 +0200

    Modified Home.jsx

--- portfolio/src/Pages/Home/Home.jsx
+++ portfolio/src/Pages/Home/Home.jsx
@@ -1,20 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: #BF4F74;
-`;
+const Container = styled.div`
+  width: 100%;
+  height :100vh;
+  background-color: #000102;
+  display: flex;
+  justify-content: center;
+  position: relative;
+`
+const ContentBack = styled.div`
+  width: 90%;
+  height: 100vh;
+  border: solid .1rem;
+  position: absolute;
+  z-index: 1000;
+  background-image: url('https://i.pinimg.com/originals/af/a9/24/afa92424317d162894bd1379823b7fdc.gif');
+  background-position: center;
+  background-size: cover;
+`
+const ContentFront = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #00ffff8d;
+  border: solid .1rem;
+  position: absolute;
+  z-index: 2000;
+`
 
 const Home = () => {
-
-
-
   return (
-    <div>
-        <Title>Home</Title>
-    </div>
+    <Container>
+      <ContentBack>
+        back
+      </ContentBack>
+
+      <ContentFront>
+        Fornt
+      </ContentFront>
+    </Container>
   )
 }
 
