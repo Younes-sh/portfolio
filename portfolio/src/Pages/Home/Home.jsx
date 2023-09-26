0844e40c7b7c4f40a6e665734bd3020ae4520a60
younes-sh <sheikhlaryounes@gmail.com>
2023-09-26 19:51:26 +0200

    Edit Home page

--- portfolio/src/Pages/Home/Home.jsx
+++ portfolio/src/Pages/Home/Home.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
-import Header from '../../Components/Header/Header';
 import {Row,Col} from 'react-bootstrap'
-
+import Header from '../../Components/Header/Header';
+import Items from '../../Components/Articles/items';
+import Main from '../../Components/Main/Main';
 
 const Home = () => {
   return (
-    <div className=' w-100 vh-100'>
-      <Row className=''>
+    <div className=' w-100 vh-100 pt-5'>
+      <Row className='pt-5'>
         <Col>
           <Header />
         </Col>
       </Row>
+      {/* Main */}
+      <Row className='pt-5 mt-5'>
+        <Col>
+          <Items />
+        </Col>
+      </Row>
      
       <br />
-
+      <Row className='pt-5 mt-5'>
+        <Col>
+          <Main />
+        </Col>
+      </Row>
       
     </div>
   )
