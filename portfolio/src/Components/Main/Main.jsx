996e42fb093fd7fdf621779880a76928a4d2cce1
younes-sh <sheikhlaryounes@gmail.com>
2023-09-26 19:49:26 +0200

    Create main

---
+++ portfolio/src/Components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CardIframe from '../Cards/CardProject';
+
+const Card = (props) => {
+  return (
+    <div className='container mt-5 pt-5'>
+        <CardIframe IMG="https://i.pinimg.com/750x/80/25/99/80259999150e2c81eb6625dcee85fb3a.jpg" />
+    </div>
+  )
+}
+
+export default Card
