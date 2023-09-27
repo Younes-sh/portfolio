652dda963173890babfcd5611e8cc2861cc12843
younes-sh <sheikhlaryounes@gmail.com>
2023-09-27 13:03:47 +0200

    Update Card

--- portfolio/src/Components/Cards/Card.jsx
+++ portfolio/src/Components/Cards/Card.jsx
@@ -5,8 +5,8 @@ import React from 'react';
 const Card = (props) => {
     
   return (
-    <div style={{width:'250px',marginTop:'10px'}}>
-      <img style={{width:'250px', height:'250px'}} src={props.IMG} alt={props.title} />
+    <div style={{width:'200px',marginTop:'10px'}}>
+      <img style={{width:'200px', height:'200px'}} src={props.IMG} alt={props.title} />
       <h4>{props.title}</h4>
       <hr />
       <span>{props.text}</span>
