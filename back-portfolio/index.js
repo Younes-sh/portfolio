748044872364962afdcfbac578da1fdbb4ee02f6
younes-sh <sheikhlaryounes@gmail.com>
2023-09-27 13:02:22 +0200

    styled glasmorphism for Card projects

--- back-portfolio/index.js
+++ back-portfolio/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express()
+const port = process.env.PORT
 
 app.get('/' , (req , res) => {
     res.send('Hi YOUNES')
 })
-app.listen(5000 ,() => console.log('server is run'))
+app.listen(port ,() => console.log('server is run'))
