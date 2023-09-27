87c97bccd8a0913508ed231a0ed221eb8c8610e2
younes-sh <sheikhlaryounes@gmail.com>
2023-09-27 14:26:46 +0200

    Update

--- portfolio/src/Components/Cards/CardProject.jsx
+++ portfolio/src/Components/Cards/CardProject.jsx
@@ -9,13 +9,13 @@ const CardProject = (props) => {
       onMouseEnter={() => setIsShown(true)}
       onMouseLeave={() => setIsShown(false)}
     >
-      <div style={{width:'400px' , height:'240px',position:'relative',display:'flex'}}>
+      <div className='box-shadow' style={{width:'400px' , height:'240px',position:'relative',display:'flex'}}>
         <img style={{width:'100%', height:'240px',position:'absolute'}} src={props.IMG} alt={props.title} />
         
         {isShown && (
-        <div className='glasmorphism text-start' style={{width:'100%',position:'absolute',marginTop:'90px'}}>
-          <h3 className='text-start mx-2 mt-3'>{props.title}Watchface</h3>
-          <span className='mx-2 mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quaerat optio laboriosam. Error odio voluptatum </span>
+        <div className='glasmorphism text-start px-3 py-3' style={{width:'100%',position:'absolute',marginTop:'90px'}}>
+          <h3 className='text-start '>{props.title}Watchface</h3>
+          <span className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quaerat optio laboriosam. Error odio voluptatum </span>
         </div>
       )}
       </div>
