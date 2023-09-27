a403659dde745ffbf4e110e01e2570d0c8ff4148
younes-sh <sheikhlaryounes@gmail.com>
2023-09-27 13:00:38 +0200

    Edite Card project

--- portfolio/src/Components/Cards/CardProject.jsx
+++ portfolio/src/Components/Cards/CardProject.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import {useState} from 'react';
 
 const CardProject = (props) => {
+  const [isShown, setIsShown] = useState(false);
+
+
   return (
-    <div style={{width:'400px'}}>
-      <img style={{width:'100%'}} src={props.IMG} alt="" />
-    </div>
+    <button style={{border:'none',background:'none'}}
+      onMouseEnter={() => setIsShown(true)}
+      onMouseLeave={() => setIsShown(false)}
+    >
+      <div style={{width:'400px' , height:'240px',position:'relative',display:'flex'}}>
+        <img style={{width:'100%', height:'240px',position:'absolute'}} src={props.IMG} alt={props.title} />
+        
+        {isShown && (
+        <div className='glasmorphism text-start' style={{width:'100%',position:'absolute',marginTop:'90px'}}>
+          <h3 className='text-start mx-2 mt-3'>{props.title}Watchface</h3>
+          <span className='mx-2 mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quaerat optio laboriosam. Error odio voluptatum </span>
+        </div>
+      )}
+      </div>
+    </button>
   )
 }
 
