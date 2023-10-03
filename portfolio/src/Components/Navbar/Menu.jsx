334ef037ed02fb28c768cd364dc3afe57f5fe4f6
younes-sh <sheikhlaryounes@gmail.com>
2023-10-03 14:08:51 +0200

    added navbar bootstrap

--- portfolio/src/Components/Navbar/Menu.jsx
+++ portfolio/src/Components/Navbar/Menu.jsx
@@ -1,48 +1,52 @@
-import {Link , NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
 import styled from 'styled-components';
-
-const UL = styled.ul`
-    display: flex;
-    list-style-type: none;
-    display:flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 9000;
-
-    li {
-        padding : 10px ;
-
-        a {
-            color: #fff;
-            text-decoration: none;
-            font-size: 1.3rem;
-        }
-
-        .active {
-            color:#fff !important;
-            border-bottom: solid .2rem;
-        }
-    }
-`
-
 
 
 const Menu = () => {
+
+  const DIV = styled.div`
+    .navbar a{
+      color:#fff;
+      marging:20px;
+      border-bottom-width: 2px ;
+    }
+    .navbar .active {
+      color:#c7e1ff;
+      border-top:solid 2px;
+    }
+  `
   return (
-    <div className='w-100 pt-4'>
-        <UL>
-            <li>
-                <NavLink to='/' >Home</NavLink>
-            </li>
-            <li>
-                <NavLink to='/about' >About</NavLink>
-            </li>
-            <li>
-                <NavLink to='/contact' >Contact</NavLink>
-            </li>
-        </UL>
-    </div>
-  )
-}
+    <DIV>
+      <Navbar className='navbar' expand="lg" style={{position:'fixed',marginTop:'-20px',marginLeft:'-200px'}}>
+            <Container className='content'>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                <NavLink className='px-3 ' style={{textDecoration:'none'}} to='/' >Home</NavLink>
+                <NavLink className='px-3 ' style={{textDecoration:'none'}} to='/projects' >Projects</NavLink>
+                <NavLink className='px-3 ' style={{textDecoration:'none'}} to='/about' >About</NavLink>
+                <NavLink className='px-3 ' style={{textDecoration:'none'}} to='/contact' >Contact</NavLink>
+                {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown> */}
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+      </Navbar>
+    </DIV>
+    )
+  }
 
 export default Menu
