9785a81d99de44e887c5c9fc5eb7af085d17c87f
younes-sh <sheikhlaryounes@gmail.com>
2023-10-03 20:27:01 +0200

    added proxy setupProxy.js

---
+++ portfolio/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api', // مسیری که می‌خواهید به بک‌اند ارتباط برقرار کنید
+    createProxyMiddleware({
+      target: 'https://back-portfolio-nine.vercel.app/', // آدرس بک‌اند شما
+      changeOrigin: true,
+    })
+  );
+};
