40bc440b95287ee9d2708b19ce749f200def53b3
younes-sh <sheikhlaryounes@gmail.com>
2023-10-03 14:08:23 +0200

    edit src/Components/Cards/CardProject.jsx

--- portfolio/src/Components/Cards/CardProject.jsx
+++ portfolio/src/Components/Cards/CardProject.jsx
@@ -1,25 +1,55 @@
 import {useState} from 'react';
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
 
 const CardProject = (props) => {
   const [isShown, setIsShown] = useState(false);
+  const Button = styled.button`
+    padding:0 5px;
+    @media (max-width: 768px) {
+      padding:0;
+    }
+  `
 
-
+  const DIV = styled.div`
+    width:400px;
+    height:240px;
+    margine:20px;
+    background-color:red;
+    hover:{
+      scale: 1.25;
+    }
+    @media (max-width: 768px) {
+      width:360px;
+      margine:20px auto;
+    }
+    
+  `
+    const Span = styled.span`
+      eight: 20px; 
+      verflow: hidden;
+    `
   return (
-    <button style={{border:'none',background:'none'}}
+    <Button className='mt-5 pt-5 ' style={{border:'none',background:'none',margin:'auto'}}
       onMouseEnter={() => setIsShown(true)}
       onMouseLeave={() => setIsShown(false)}
     >
-      <div className='box-shadow' style={{width:'400px' , height:'240px',position:'relative',display:'flex'}}>
-        <img style={{width:'100%', height:'240px',position:'absolute'}} src={props.IMG} alt={props.title} />
+      <DIV className='box-shadow ' style={{ position:'relative',display:'flex'}}>
+        <img style={{width:'100%', height:'240px',position:'absolute'}} src={props.UrlImage} alt={props.title} />
         
         {isShown && (
         <div className='glasmorphism text-start px-3 py-3' style={{width:'100%',position:'absolute',marginTop:'90px'}}>
           <h3 className='text-start '>{props.title}Watchface</h3>
-          <span className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quaerat optio laboriosam. Error odio voluptatum </span>
+          <Span className='description'style={{overflow:'hidden'}}>{props.details}</Span>
+
+          <div className='d-flex justify-content-between pt-2'>
+            <Link to={`/projectparams/${props._id}`}className='btn btn-info'>info</Link>
+            <Link target='_blank' to={`${props.directLink}`} className='btn btn-success'>View site</Link>
+          </div>
         </div>
       )}
-      </div>
-    </button>
+      </DIV>
+    </Button>
   )
 }
 
