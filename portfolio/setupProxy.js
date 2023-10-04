e272b57db9201df1d622cb5976e8c0ba6b4dd285
younes-sh <sheikhlaryounes@gmail.com>
2023-10-04 11:11:34 +0200

    unComment setupProxy

--- portfolio/setupProxy.js
+++ portfolio/setupProxy.js
@@ -1,24 +1,24 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// module.exports = function(app) {
-//   // ارتباط با بک‌اند اول
-//   app.use(
-//     '/api/backend1', // مسیری که می‌خواهید به بک‌اند اول ارتباط برقرار کنید
-//     createProxyMiddleware({
-//       target: 'https://back-portfolio-nine.vercel.app/', // آدرس بک‌اند اول
-//       changeOrigin: true,
-//     })
-//   );
+module.exports = function(app) {
+  // ارتباط با بک‌اند اول
+  app.use(
+    '/api/backend1', // مسیری که می‌خواهید به بک‌اند اول ارتباط برقرار کنید
+    createProxyMiddleware({
+      target: 'https://back-portfolio-nine.vercel.app/', // آدرس بک‌اند اول
+      changeOrigin: true,
+    })
+  );
 
-//   // ارتباط با بک‌اند دوم
-//   app.use(
-//     '/api/backend2', // مسیری که می‌خواهید به بک‌اند دوم ارتباط برقرار کنید
-//     createProxyMiddleware({
-//       target: 'http://localhost:5000', // آدرس بک‌اند دوم
-//       changeOrigin: true,
-//     })
-//   );
-// };
+  // ارتباط با بک‌اند دوم
+  app.use(
+    '/api/backend2', // مسیری که می‌خواهید به بک‌اند دوم ارتباط برقرار کنید
+    createProxyMiddleware({
+      target: 'http://localhost:5000', // آدرس بک‌اند دوم
+      changeOrigin: true,
+    })
+  );
+};
 
 
 // 'https://younessheikhlar.vercel.app',
