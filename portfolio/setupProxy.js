8608e6c85a7d0dae618152a3c9ef6f0cca717de0
younes-sh <sheikhlaryounes@gmail.com>
2023-10-04 11:10:17 +0200

    added setupProxy

--- portfolio/setupProxy.js
+++ portfolio/setupProxy.js
@@ -1,11 +1,24 @@
 // const { createProxyMiddleware } = require('http-proxy-middleware');
 
 // module.exports = function(app) {
+//   // ارتباط با بک‌اند اول
 //   app.use(
-//     '/api', // مسیری که می‌خواهید به بک‌اند ارتباط برقرار کنید
+//     '/api/backend1', // مسیری که می‌خواهید به بک‌اند اول ارتباط برقرار کنید
 //     createProxyMiddleware({
-//       target: ['https://back-portfolio-nine.vercel.app/' , 'http://localhost:5000'], // آدرس بک‌اند شما
+//       target: 'https://back-portfolio-nine.vercel.app/', // آدرس بک‌اند اول
+//       changeOrigin: true,
+//     })
+//   );
+
+//   // ارتباط با بک‌اند دوم
+//   app.use(
+//     '/api/backend2', // مسیری که می‌خواهید به بک‌اند دوم ارتباط برقرار کنید
+//     createProxyMiddleware({
+//       target: 'http://localhost:5000', // آدرس بک‌اند دوم
 //       changeOrigin: true,
 //     })
 //   );
 // };
+
+
+// 'https://younessheikhlar.vercel.app',
