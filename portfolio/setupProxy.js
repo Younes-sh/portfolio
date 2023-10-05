7c6b8c5572a221d429938e51ce7f296d031be706
younes-sh <sheikhlaryounes@gmail.com>
2023-10-05 11:44:08 +0200

    Update setupProxy

--- portfolio/setupProxy.js
+++ portfolio/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   // ارتباط با بک‌اند اول
   app.use(
-    '/api/backend1', // مسیری که می‌خواهید به بک‌اند اول ارتباط برقرار کنید
+    '/api', // مسیری که می‌خواهید به بک‌اند اول ارتباط برقرار کنید
     createProxyMiddleware({
       target: 'https://portfolio-d-iota.vercel.app', // آدرس بک‌اند اول
       changeOrigin: true,
