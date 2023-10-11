230eb5b762d6a4d4af6dd16d4212e7de4ff09fdf
younes-sh <sheikhlaryounes@gmail.com>
2023-10-11 15:36:46 +0200

    added Spinner on Projects page

--- portfolio/src/Pages/Projects/Projects.jsx
+++ portfolio/src/Pages/Projects/Projects.jsx
@@ -1,22 +1,31 @@
 import {useState , useEffect} from 'react';
 import CardProject from '../../Components/Cards/CardProject';
+import Spinner  from '../../Components/Spiner/Spiner';
 
 const Projects = () => {
     const [ items , setItems ] = useState([]);
+    const [loading, setLoading] = useState(true); // اضافه کردن متغیر loading
+
     const URL_API = process.env.REACT_APP_URL_API;
     useEffect(() => {
-        fetch(`${URL_API}/api/projects`)
-        .then(res => res.json())
-        .then(res => setItems(res.data))
-        .catch(error => console.log(error))
-    },[])
+      fetch(`${URL_API}/api/projects`)
+          .then(res => res.json())
+          .then(res => {
+              setItems(res.data);
+              setLoading(false); // دیگر در حالت بارگیری نیست
+          })
+          .catch(error => console.log(error));
+  }, []);
   return (
     <div className='w-100 100-vh pt-5'>
-        <h1 className='pt-5'>Projects</h1>
         <div className='container 100-vh my-3 d-flex flex-wrap'>
-        {
-          items.map(item => <CardProject key={items} {...item}/>)
-        }
+        {loading ? ( // اگر در حال بارگیری هستیم، اسپینر را نمایش بده
+                    <div className='w-100 100-vh mt-5 pt-5 d-flex justify-content-center align-items-center'>
+                      <Spinner animation="border" />
+                    </div>
+                ) : (
+                    items.map(item => <CardProject key={item.id} {...item} />)
+                )}
         </div>
     </div>
   )
