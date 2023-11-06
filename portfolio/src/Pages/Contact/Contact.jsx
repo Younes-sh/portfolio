2a5462971422da0cdf62b23a3238a085661a68db
younes-sh <sheikhlaryounes@gmail.com>
2023-11-06 17:49:19 +0100

    Edit contact page

--- portfolio/src/Pages/Contact/Contact.jsx
+++ portfolio/src/Pages/Contact/Contact.jsx
@@ -1,19 +1,56 @@
-import React from 'react';
+import {useState} from 'react';
 import './style.css';
 import {Row,Col} from 'react-bootstrap';
-import CardContact from '../../Components/Cards/CardContact';
+import Github from '../../assets/Contact/github.png';
+import Diribbble from '../../assets/Contact/dribbble.png';
+import Linkedin from '../../assets/Contact/linkedin.png';
+import Pinterest from '../../assets/Contact/pinterest.png';
+import Location from '../../assets/Contact/location.png';
+import Email from '../../assets/Contact/email.png';
 
 const Contact = () => {
   return (
-    <div className='Contact pt-5 d-flex flex-column '>
-      <div className='w-100 m-auto px-5 d-flex flex-column'>
-        <div  className='Child '>
-          <CardContact title={'GitHub'} link={'https://github.com/Younes-sh'} IMG='https://user-images.githubusercontent.com/74038190/212741999-016fddbd-617a-4448-8042-0ecf907aea25.gif' />
-        </div>
-        <div  className='Child '>
-        <CardContact title={'LinkedIn'} link={'https://www.linkedin.com/in/younes-sheikhlar/'} IMG='https://cdn.pixabay.com/photo/2018/05/02/12/02/linkedin-3368467_1280.jpg' />
-        </div>
+    <div className='Contact  d-flex align-items-center  pt-sm-5'>
+      <div className='container   pt-sm-5'>
+        <Row className=' text-light  lg-flex-row'>
+        <Col lg={4} className='d-flex flex-column text-start' >
+          <h5>CONTACT INFO</h5>
+          <div className='d-flex flex-lg-column justify-content-sm-around containerContact '>
+            <div className=''>
+              <img src={Email} alt="email" />
+              <span> <a style={{textDecoration:'none', color:'#fff'}} href="mailto:sheikhlaryounes@gmail.com">sheikhlaryounes@gmail.com</a></span>
+            </div>
+            
+            <div className=''>
+              <img style={{color:'#ccc'}} src={Location} alt="Location" />
+              <span> Belgium, Brussels</span>
+            </div>
+
+          </div>
+          <div className='mt-5'>
+            <h5>SOCIAL INFO</h5>
+            <Col className='pt-2'>
+              <a  className='' target='_blank' href="https://github.com/Younes-sh"><img src={Github} alt='github'></img></a>
+              <a  className='mx-2' target='_blank' href="https://dribbble.com/younessh"><img src={Diribbble} alt='github'></img></a>
+              <a  className='' target='_blank' href="https://www.linkedin.com/in/younes-sheikhlar/"><img src={Linkedin} alt='Linkedin'></img></a>
+              <a  className='mx-2' target='_blank' href="https://www.pinterest.com/sheikhlaryounes/"><img src={Pinterest} alt='pinterest'></img></a>
+            </Col>
+
+          </div>
+        </Col>
+        {/*  */}
+        <Col lg={8} className='mt-5  mt-lg-0'>
+              <div className='d-flex flex-column border-2 formContact mt-5 mt-lg-0'>
+                <h1>Let's work <span style={{color:'#EC8106'}}>together</span></h1>
+                <input className='textinput' type="text" placeholder='Name *'/>
+                <input className='textinput' type="text" placeholder='Email *'/>
+                <input className='textinput' type="text" placeholder='Your subject *' />
+                <textarea className='textinput' type="text" rows={7} />
+                <button>Send Message</button>
+              </div>
+        </Col>
         
+        </Row>
       </div>
     </div>
   )
