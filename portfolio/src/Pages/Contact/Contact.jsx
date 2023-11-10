2931f279fd6672330363865988a4f3bc2a023f40
younes-sh <sheikhlaryounes@gmail.com>
2023-11-10 15:08:21 +0100

    Edite Contact page and Change Form

--- portfolio/src/Pages/Contact/Contact.jsx
+++ portfolio/src/Pages/Contact/Contact.jsx
@@ -1,5 +1,5 @@
-import {useState} from 'react';
 import './style.css';
+import Form from './Form';
 import {Row,Col} from 'react-bootstrap';
 import Github from '../../assets/Contact/github.png';
 import Diribbble from '../../assets/Contact/dribbble.png';
@@ -9,57 +9,50 @@ import Location from '../../assets/Contact/location.png';
 import Email from '../../assets/Contact/email.png';
 
 const Contact = () => {
-  const [data, setData] = useState({});
 
-  const controlHndler = (e) => {
-    setData({...data, [e.target.name ] : e.target.value})
-  }
-
-  const senHandler = () => {
-
-  }
   return (
-    <div className='Contact  d-flex align-items-center  pt-sm-5'>
-      <div className='container   pt-sm-5'>
-        <Row className=' text-light  lg-flex-row'>
-        <Col lg={4} className='d-flex flex-column text-start' >
-          <h5>CONTACT INFO</h5>
-          <div className='d-flex flex-lg-column justify-content-sm-around containerContact '>
-            <div className=''>
-              <img src={Email} alt="email" />
-              <span> <a style={{textDecoration:'none', color:'#fff'}} href="mailto:sheikhlaryounes@gmail.com">sheikhlaryounes@gmail.com</a></span>
-            </div>
-            
-            <div className=''>
-              <img style={{color:'#ccc'}} src={Location} alt="Location" />
-              <span> Belgium, Brussels</span>
-            </div>
+    <div className='Contact   d-flex  pt-5 text-light'>
+      <div className='contect vh-100  h-sm-100  d-flex   mt-5 pt-5'>
+        <div className='w-100 d-lg-flex flex-wrap  justify-content-center  text-start'>
+          <div  className='d-flex flex-lg-column  align-items-center w-sm-100  w-lg-25 ' >
+            <div className='d-flex flex-column justify-content-sm-around  align-items-center align-items-lg-start mx-auto   containerContact'>
+            <h5>CONTACT INFO</h5>
+              <div className='d-flex    flex-lg-column m-auto flex-md-row  flex-sm-row flex-xs-row '>
 
-          </div>
-          <div className='mt-5'>
-            <h5>SOCIAL INFO</h5>
-            <Col className='pt-2'>
-              <a  className='' target='_blank' href="https://github.com/Younes-sh"><img src={Github} alt='github'></img></a>
-              <a  className='mx-2' target='_blank' href="https://dribbble.com/younessh"><img src={Diribbble} alt='github'></img></a>
-              <a  className='' target='_blank' href="https://www.linkedin.com/in/younes-sheikhlar/"><img src={Linkedin} alt='Linkedin'></img></a>
-              <a  className='mx-2' target='_blank' href="https://www.pinterest.com/sheikhlaryounes/"><img src={Pinterest} alt='pinterest'></img></a>
-            </Col>
+                <div >
+                  <img className='w-sm-25' src={Email} alt="email" />
+                  <span> <a style={{textDecoration:'none', color:'#fff'}} href="mailto:sheikhlaryounes@gmail.com"> sheikhlaryounes@gmail.com</a></span>
+                </div>
 
-          </div>
-        </Col>
-        {/*  */}
-        <Col lg={8} className='mt-5  mt-lg-0'>
-              <div className='d-flex flex-column border-2 formContact mt-5 mt-lg-0'>
-                <h1>Let's work <span style={{color:'#EC8106'}}>together</span></h1>
-                <input onChange={(e) => controlHndler(e , 'name')} name='name' className='textinput' type="text" placeholder='Name *'/>
-                <input onChange={(e) => controlHndler(e , 'email')} name='email' className='textinput' type="text" placeholder='Email *'/>
-                <input onChange={(e) => controlHndler(e , 'subject')} name='subject' className='textinput' type="text" placeholder='Your subject *' />
-                <textarea onChange={(e) => controlHndler(e , 'message')} name='message' className='textinput' type="text" rows={7}  placeholder='Message'></textarea>
-                <button onClick={senHandler} type='button'>Send Message</button>
+                <div className='mx-md-2 mx-sm-2 mx-lg-0  '>
+                  <img className='w-sm-25' style={{color:'#ccc'}} src={Location} alt="Location" />
+                  <span> Belgium, Brussels</span>
+                </div>
               </div>
-        </Col>
+
+
+              <div className='mt-5 pt-5 text-start '>
+                <h5>SOCIAL INFO</h5>
+                <a  className='' target='_blank' href="https://github.com/Younes-sh"><img src={Github} alt='github'></img></a>
+                <a  className='mx-2' target='_blank' href="https://dribbble.com/younessh"><img src={Diribbble} alt='github'></img></a>
+                <a  className='' target='_blank' href="https://www.linkedin.com/in/younes-sheikhlar/"><img src={Linkedin} alt='Linkedin'></img></a>
+                <a  className='mx-2' target='_blank' href="https://www.pinterest.com/sheikhlaryounes/"><img src={Pinterest} alt='pinterest'></img></a>
+              </div>
+            </div>
+
+          </div>
+
+          {/* Form */}
+
+
+          <div  className='w-md-100 w-lg-75 mt-sm-5 mt-md-5 d-flex justify-content-sm-center   justify-content-md-center justify-content-lg-end mt-lg-0 '>
+            <div className='d-flex flex-column  formContact mt-5 mt-lg-0 bg-danger'>
+              <p className='fs-3'>Let's work <span style={{color:'#EC8106'}}>together</span></p>
+              <Form />
+            </div>
+          </div>
         
-        </Row>
+        </div>
       </div>
     </div>
   )
