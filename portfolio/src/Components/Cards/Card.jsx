bcf580ca082ea4aa739d8ed7834c989d9f78b3c9
younes-sh <sheikhlaryounes@gmail.com>
2023-12-28 17:32:00 +0100

    Edit Card

--- portfolio/src/Components/Cards/Card.jsx
+++ portfolio/src/Components/Cards/Card.jsx
@@ -4,9 +4,8 @@ const Card = (props) => {
   return (
     <div style={{width:'200px',marginTop:'10px'}}>
       <img style={{width:'200px', height:'200px'}} src={props.IMG} alt={props.title} />
-      <h5>{props.title}</h5>
-      <hr />
-      <span style={{whiteSpace:'break-spaces'}} >{props.text}</span>
+      <h5 className="text-light">{props.title}</h5>
+      <span style={{whiteSpace:'break-spaces', color:'#ccc'}} >{props.text}</span>
     </div>
   )
 }
