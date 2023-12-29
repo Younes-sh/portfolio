7175c8f49447373985675b734ab6082477fd298c
younes-sh <sheikhlaryounes@gmail.com>
2023-12-29 15:07:08 +0100

    Updated

--- portfolio/src/Components/Home/Main/MainOne.jsx
+++ portfolio/src/Components/Home/Main/MainOne.jsx
@@ -3,33 +3,24 @@ import styled  from 'styled-components';
 import './main.css'
 import Shape from '../../../assets/Home/Subtract.svg';
 import {Row , Col} from 'react-bootstrap';
-import { motion } from "framer-motion";
 
 const Main = () => {
   return (
     <div className='container 100-vh my-5 mx-auto py-5 d-flex flex-column justify-content-center align-items-center '>
-      <motion.section className='scroll-reveal d-flex flex-column justify-content-center align-items-center'
+      <section className='scroll-reveal d-flex flex-column justify-content-center align-items-center'
         animate={{ backgroundSize: '100% 100%' }}
         transition={{ duration: 1 }}
       >
-        <motion.h2 className=''
-          initial={{ backgroundSize: '0% 100%' }}
-          animate={{ backgroundSize: '100% 100%' }}
-          transition={{ duration: 1 }}
-        >
+        <h2 className=''>
          A Stunning Outlook on Front-End World
-        </motion.h2>
-        <motion.p className='mt-5' style={{maxWidth:'600px'}}
-          initial={{ backgroundSize: '0% 100%' }}
-          animate={{ backgroundSize: '100% 100%' }}
-          transition={{ duration: 1 }}
-        >
+        </h2>
+        <p className='mt-5' style={{maxWidth:'600px'}}>
           
           <span>
             As a front-end enthusiast, I take pride in sharing my digital creations a fusion of beauty and functionality that elevates user experience to new heights. My aim here is to present a credible and innovative perspective on the digital realm. I'm thrilled to have you accompany me on this journey.
           </span>
-        </motion.p>
-      </motion.section>
+        </p>
+      </section>
 
       <Row className='w-100  mt-5  pt-5  align-items-center'>
         <Col lg={6} className=' d-flex justify-content-center'>
